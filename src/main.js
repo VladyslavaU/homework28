e43dfd45605f7dff0431fb2730da6464e5529ce1
VLADYSLAVA UVAROVA <vuvarova@outlook.com>
2024-05-03 23:17:09 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,38 +1,11 @@
 console.log('#13. JavaScript homework example file')
 
-/*
- * #1
- *
- * Написати функцію, яка приймає рядок як вхідний параметр і перевіряє, чи є цей рядок валідною електронною адресою за допомогою регулярного виразу.
- * Функція повертає true, якщо електронна адреса валідна, і false в іншому випадку.
- *
- */
-
 function isValidEmail(email) {
-  // code here
+  return /^[^\s@]+@[^\s@]+\.[^\s@]{2,4}$/.test(email);
 }
-
-// console.log(isValidEmail('example@example.com')) // Повинно вивести: true
-// console.log(isValidEmail('invalid-email'))       // Повинно вивести: false
-
-/*
- * #2
- *
- * Написати функцію, яка приймає рядок як вхідний параметр і перевіряє, чи є цей рядок валідним URL веб-сайту за допомогою регулярного виразу.
- * Функція повертає true, якщо URL валідний, і false в іншому випадку.
- *
- */
-
-/*
-
-*/
 
 function isValidUrl(url) {
-  // code here
+  return /^(?:https?|ftp):\/\/(?:\S+(?::\S*)?@)?(?:[\w-]+\.)+[a-z]{2,}(?::\d{1,5})?(?:\/[^\s]*)?$/i.test(url);
 }
 
-// console.log(isValidUrl('https://www.example.com')) // Повинно вивести: true
-// console.log(isValidUrl('invalid-url'))             // Повинно вивести: false
-
-// Експорт функції для використання та тестування
 export { isValidEmail, isValidUrl }
